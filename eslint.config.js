8c423981121d38a80139e8ddd2eb5a1d34c8703e
Jordi Campeny <jordicampeny12@gmail.com>
2025-01-31 20:40:24 +0100

    add eslint-plugin-cypress

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import cypress from 'eslint-plugin-cypress'
 
 export default [
   { ignores: ['dist'] },
@@ -10,7 +11,9 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser, // For regular browser globals
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -33,6 +36,23 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+    },
+  },
+  {
+    files: ['cypress/**/*.spec.{js,jsx,ts,tsx}', '**/*.cy.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.mocha,
+        Cypress: 'readonly',
+        cy: 'readonly',
+      },
+    },
+    plugins: {
+      cypress,
+    },
+    rules: {
+      'no-unused-expressions': 'off', // Disable for Cypress assertions like `expect(...).to.be.true`
     },
   },
 ]
